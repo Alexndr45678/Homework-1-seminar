d2829c9ee016a4bc39d839a32d23d08adf22e3f4
Alexandr <koshin97@mail.ru>
2022-08-27 23:03:43 +0300

    Completed the second task

---
+++ Задача2/Program.cs
@@ -0,0 +1,31 @@
+﻿// По заданному номеру дня недели вывести его название.
+
+Console.Write("Введите номер дня недели: ");
+                try
+                {
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    switch (a)
+                    {
+                        case 1: Console.WriteLine("Понедельник");
+                            break;
+                        case 2: Console.WriteLine("Вторник");
+                            break;
+                        case 3: Console.WriteLine("Среда");
+                            break;
+                        case 4: Console.WriteLine("Четверг");
+                            break;
+                        case 5: Console.WriteLine("Пятница");
+                            break;
+                        case 6: Console.WriteLine("Суббота");
+                            break;
+                        case 7: Console.WriteLine("Воскресенье");
+                            break;
+                        default:
+                            Console.WriteLine("Такой номер дня недели не существует");
+                            break;
+                    }
+                }
+            catch(FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
