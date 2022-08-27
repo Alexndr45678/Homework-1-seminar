15510b1745175cda32bce5e4213434a3d5f331bc
Alexandr <koshin97@mail.ru>
2022-08-27 22:06:32 +0300

    Comleted task 1

---
+++ Задача1/Program.cs
@@ -0,0 +1,13 @@
+﻿// По заданным двум числам проверять является ли число "a" квадратом числа "b".
+Console.WriteLine("Введите число: ");
+double a = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32(Console.ReadLine());
+
+if (b == a * a)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
