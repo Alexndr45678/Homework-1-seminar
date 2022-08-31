d86527660477b02de9d4a2c80c07b962a5fdff0f
Alexandr <koshin97@mail.ru>
2022-08-31 13:23:59 +0300

    Compltask7

---
+++ Задача7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трехзначного числа.
+
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(a);
+Console.WriteLine(stringNumber[1]);
