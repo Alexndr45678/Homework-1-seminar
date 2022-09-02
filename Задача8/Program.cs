f0327ef1ceb1942672f05a2e898654417abfe866
Alexandr <koshin97@mail.ru>
2022-09-02 17:56:10 +0300

    correctedtask8

--- Задача8/Program.cs
+++ Задача8/Program.cs
@@ -1,6 +1,6 @@
 ﻿// удалить вторую цифру трехзначного числа.
 
 int a = new Random().Next(100, 999);
-Console.WriteLine(Convert.ToString(a));
+Console.WriteLine(a);
 string number = a.ToString();
 Console.WriteLine(number.Remove(1, 1));
