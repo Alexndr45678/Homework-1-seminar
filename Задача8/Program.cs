108a6d1a2931e0756636e16a09d1886080736dd4
Alexandr <koshin97@mail.ru>
2022-09-02 17:45:50 +0300

    completetask8

---
+++ Задача8/Program.cs
@@ -0,0 +1,6 @@
+﻿// удалить вторую цифру трехзначного числа.
+
+int a = new Random().Next(100, 999);
+Console.WriteLine(Convert.ToString(a));
+string number = a.ToString();
+Console.WriteLine(number.Remove(1, 1));
