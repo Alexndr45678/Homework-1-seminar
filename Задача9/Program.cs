75afa04cb304c1ba5b57b9cb82adb238caad9072
Alexandr <koshin97@mail.ru>
2022-09-05 21:44:27 +0300

    completetask9

---
+++ Задача9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+double a = 125;
+double b = 30;
+bool c = (a % b == 0);
+Console.WriteLine(c);
+Convert.ToInt64(c);
+if (c == false)
+{
+    Console.WriteLine(a % b);
+}
