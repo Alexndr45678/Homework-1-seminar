ffafed02ad1d8c47a1a72aefc6c99ae7ba4d1456
Alexandr <koshin97@mail.ru>
2022-09-06 10:21:10 +0300

    completetask10

---
+++ Задача10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = (int)Math.Log10(a)-2;
+Console.WriteLine(b < 0 ? "Третьей цифры нет." : (a % (int)Math.Pow(10, b + 1) / (int)Math.Pow(10, b)).ToString());
